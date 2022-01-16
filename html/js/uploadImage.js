708ba3555e00a8cccef774846db397802a490054
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2022-01-16 12:24:37 -0500

    Final changes

--- html/js/uploadImage.js
+++ html/js/uploadImage.js
@@ -6,24 +6,24 @@ const MAX_IMAGE_SIZE = 40000000;
 
 function handleUploadImage(email, lat, lon, timestamp) {
     const form = document.uploadImageForm;
-    form.uploadButton.disabled = true;
+    document.getElementById("uploadButton").disabled = true;
 
     let data = {};
     // TODO extract email of uploader
-    data["creator"] = "test2@wpi.edu";
+    data["creator"] = email;
     data["name"] = document.querySelector('input[type=file]').files[0].name;
-    data["lat"] = 47;
-    data["lon"] = -71;
-    data["timestamp"] = 1642291000;
+    data["lat"] = lat;
+    data["lon"] = lon;
+    data["timestamp"] = timestamp;
 
     // Removes preamble which states this is a base64 value
-    const segments = form.base64EncodedValue.value.split(',');
+    const segments = document.getElementById("base64Encoding").value.split(',');
     data["base64encoded"] = segments[1];
 
     let js = JSON.stringify(data);
     console.log("JS:" + js);
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", 'https://seekersapi1-0-1pwxcx8v.uk.gateway.dev/upload_image', true);
+    xhr.open("POST", 'https://us-east4-still-tensor-338300.cloudfunctions.net/uploadImage', true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(js);
 
@@ -49,7 +49,7 @@ function handleUploadImage(email, lat, lon, timestamp) {
 function handleFileSelect(evt) {
     const files = evt.target.files;
     if (files[0].size > MAX_IMAGE_SIZE) {
-        document.uploadImageForm.base64EncodedValue.value = "";
+        document.getElementById("base64Encoding").value = "";
         alert("File size too large to use: " + files[0].size + " bytes");
     } else {
         // TODO Run Metadata checks here
@@ -57,8 +57,8 @@ function handleFileSelect(evt) {
         reader.readAsDataURL(files[0]);
 
         reader.onload = function () {
-            document.uploadImageForm.base64EncodedValue.value = reader.result;
-            document.uploadImageForm.uploadButton.disabled = false;
+            document.getElementById("base64Encoding").value = reader.result;
+            document.getElementById("uploadButton").disabled = false;
         }
     }
 }
